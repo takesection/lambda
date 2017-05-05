a4199cb0210efe11963af2e57447699989026bef
Shigeki Shoji <takesection@gmail.com>
2017-05-05 14:47:01 +0900

    upgrade junit-interface

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val lambda = "com.amazonaws" % "aws-lambda-java-core" % "1.1.0"
 
   val junit = "junit" % "junit" % "4.12"
-  val junitInterface = "com.novocode" % "junit-interface" % "0.11-RC1"
+  val junitInterface = "com.novocode" % "junit-interface" % "0.11"
 
   val commonDeps = Seq(
     junit % Test,
